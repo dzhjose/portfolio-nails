a704ecc2982c62012b8f6235f5f0ce0ab67eb673
Jose Diaz <dzjose.root@gmail.com>
2026-03-04 18:29:25 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dzhjose.github.io',
+  base: '/portfolio-nails/',
+  trailingSlash: "always",
+  output: "static",
   vite: {
     plugins: [tailwindcss()]
   },
-  output: "static",
 });
